e147b8ff2b205f85f01582520d5a3e3c23ea31ad
anihmus <andrew.pomponio@gmail.com>
2024-05-19 10:37:22 -0600

    Minor update

--- backup_script.sh
+++ backup_script.sh
@@ -12,6 +12,6 @@ echo "Hello ${USER^}, I will now back up your home directory, $HOME"
 echo "You are running this script from ${currentdir} so I will put your backup in ${currentdir}."
 
 
-tar -cf $currentdir/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
+tar -cf $currentdir/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar $HOME/* 2>/dev/null
 
 echo "Backup has completed successfully."
