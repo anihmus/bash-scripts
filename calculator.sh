fca9a84c2922e53350a2d9763bf5670f9e89124d
anihmus <andrew.pomponio@gmail.com>
2024-06-17 10:09:13 -0600

    Adding calculator script project

---
+++ calculator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+$results=(*[+ - * /]*)
+echo "Enter your math problem:"
+read $type
+
+if $type=('*'[+]'*')
+    then echo "The answer is: $results"
+fi
+
+if $type=(*[-]*)
+    then echo "The answer is: $results"
+fi
+
+if $type=('*'[*]'*')
+    then echo "The answer is: $results"
+fi
+
+if $type=('*'[/]'*')
+    then echo "The answer is: $results"
+fi
